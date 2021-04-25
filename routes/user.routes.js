204c5d25f1e17a619101b0ac3c1edccd832d00bd
Fetra01 <fetra.andria@outlook.fr>
2021-04-25 17:09:46 +0200

    back-end fonctionnel

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,16 +2,25 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
+const multer = require('multer');
+const upload = multer();
 
-//création
+//auth
 router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
 
 //update, delete,get
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.userInfo);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
-router.patch('/follow/:id', userController.follow);
-router.patch('/unfollow/:id', userController.unfollow);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
+
+
+// upload
+//router.post("/upload", upload.single("file"), uploadController.uploadProfil);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,25 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
+const {checkUser, requireAuth} = require ('./middleware/auth.middleware');
+const cookieParser = require('cookie-parser');
 require("dotenv").config({path:'./config/.env'});
 require('./config/db');
+
 const app = express();
 
 const port = process.env.PORT;
 
 const corsOptions = {
     origin: process.env.REACT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
 }
 
 app.use(
     cors(corsOptions),
+    cookieParser(),
     //bodyParser.json(),
     //bodyParser.urlencoded({extended: true}),
     express.json()
 );
 
+//jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id);
+})
+
 //ROUTES
 app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 
 //SERVER
